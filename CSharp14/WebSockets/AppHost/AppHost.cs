5578c5c471404906049f6038a632a207365edb95
Rainer Stropek <rainer@software-architects.at>
2025-11-20 07:57:57 +0100

    Enhance WebSockets sample

--- CSharp14/WebSockets/AppHost/AppHost.cs
+++ CSharp14/WebSockets/AppHost/AppHost.cs
@@ -1,6 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var webapi = builder.AddProject<Projects.WebApi>("webapi");
+var sqlite = builder.AddSqlite(
+    "database",
+    builder.Configuration["Database:path"],
+    builder.Configuration["Database:fileName"])
+    .WithSqliteWeb(); // optionally add web admin UI (requires Docker/podman)
+
+var webapi = builder.AddProject<Projects.WebApi>("webapi")
+    .WithEnvironment("ConnectionStrings__Database", $"Data Source={Path.Combine(
+        builder.Configuration["Database:path"]!, 
+        builder.Configuration["Database:fileName"]!)}");
 
 // Aspire 13 has been fundamentally redesigned in the context of .NET 10.
 // Example: Integration of JavaScript is now more complete than before.
--- CSharp14/WebSockets/WebApi/Program.cs
+++ CSharp14/WebSockets/WebApi/Program.cs
@@ -1,12 +1,10 @@
-using System.Net;
-using System.Net.WebSockets;
-using System.Text;
-using System.Text.Json;
 using Microsoft.AspNetCore.WebSockets;
+using WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.AddSqliteDbContext<ApplicationDataContext>("database");
 builder.AddServiceDefaults();
 builder.Services.AddCors(options =>
 {
@@ -26,35 +24,6 @@ app.UseHttpsRedirection();
 app.UseWebSockets();
 app.MapOpenApi();
 app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
-
-app.Map("/ws", async context =>
-{
-    if (context.WebSockets.IsWebSocketRequest)
-    {
-        using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-
-        // Note the use of WebSocketStream. The stream abstraction on top of WebSocket
-        // is brand new in .NET 10/C# 14 and makes working with WebSockets much easier.
-        // It allows you to use StreamReader/StreamWriter or any other stream-based API
-        // on top of WebSockets.
-        using var stream = WebSocketStream.Create(webSocket, WebSocketMessageType.Text, true);
-        using var reader = new StreamReader(stream, Encoding.UTF8);
-        string? message;
-        while ((message = await reader.ReadLineAsync()) != null)
-        {
-            // Here we are using System.Text.Json to serialize a JSON object.
-            // We make use of the new WebSocketStream to write directly to the WebSocket.
-            await JsonSerializer.SerializeAsync(stream, new { echo = message });
-            if (message.Equals("exit", StringComparison.OrdinalIgnoreCase))
-            {
-                break;
-            }   
-        }
-    }
-    else
-    {
-        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-    }
-});
+app.MapWebSocketEndpoints();
 
 app.Run();
